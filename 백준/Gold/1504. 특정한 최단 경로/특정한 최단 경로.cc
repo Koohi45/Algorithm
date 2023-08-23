9d9d97bb0a00a9d330dc8c9b7cedc5e8f6769709
Koohi45 <86870312+Koohi45@users.noreply.github.com>
2023-08-23 16:51:28 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 132 ms, Memory: 5588 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -3,59 +3,55 @@
 #include <queue>
 
 using namespace std;
+
 typedef pair<int, int> ci;
 const int INF = 1e5 * 8 * 3;
 
 vector<int> dijkstra(int start, int n, vector<vector<ci>> &adjList){
   vector<int> dist(n + 1, INF);
-  priority_queue<ci, vector<ci>, greater<>> pq;
+  priority_queue<ci, vector<ci>, greater<>> q;
 
   dist[start] = 0;
-  pq.push({0, start});
+  q.push({0, start});
+  while(!q.empty()){
+    int v = q.top().second;
+    int w = q.top().first;
+    q.pop();
 
-  while(!pq.empty()){
-    int node = pq.top().second;
-    int weight = pq.top().first;
-    pq.pop();
-
-    if(weight > dist[node]){
+    if(w > dist[v]){
       continue;
     }
 
-    for(int i = 0; i < adjList[node].size(); i++){
-      int nextNode = adjList[node][i].first;
-      int nextWeight = weight + adjList[node][i].second;
-      if(nextWeight < dist[nextNode]){
-        dist[nextNode] = nextWeight;
-        pq.push({nextWeight, nextNode});
+    for(int i = 0; i < adjList[v].size(); i++){
+      int next = adjList[v][i].first;
+      int nextWeight = w + adjList[v][i].second;
+      if(nextWeight < dist[next]){
+        dist[next] = nextWeight;
+        q.push({nextWeight, next});
       }
     }
   }
   return dist;
 }
 
-
-
 int main(){
   int n, e, a, b, c, v1, v2;
   cin >> n >> e;
-
   vector<vector<ci>> adjList(n + 1, vector<ci>(0));
-  while(e--){
+  for(int i = 0; i < e; i++){
     cin >> a >> b >> c;
     adjList[a].push_back({b, c});
     adjList[b].push_back({a, c});
   }
   cin >> v1 >> v2;
-  
-  vector<int> startNodes = {1, v1, v2};
-  vector<vector<int>> dist(3, vector<int>(n + 1, 0));
-  for(int i = 0; i < 3; i++){
-    dist[i] = dijkstra(startNodes[i], n, adjList);
-  }
-  
-  int ans = min(dist[0][v1] + dist[1][v2] + dist[2][n], dist[0][v2] + dist[2][v1] + dist[1][n]);
 
-  cout << (ans >= INF? -1 : ans);
+  vector<int> start = {1, v1, v2};
+  vector<vector<int>> dist;
+  for(int i = 0; i < 3; i++){
+    dist.push_back(dijkstra(start[i], n, adjList));
+  }
+  int ans1 = dist[0][v1] + dist[1][v2] + dist[2][n];
+  int ans2 = dist[0][v2] + dist[2][v1] + dist[1][n];
+  cout << (ans1 >= INF && ans2 >= INF? -1 : min(ans1, ans2));
   return 0;
 }
