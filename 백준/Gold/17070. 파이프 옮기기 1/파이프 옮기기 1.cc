4204857d189c7feda19332d8bb62f9b5c4a1ac48
Koohi45 <86870312+Koohi45@users.noreply.github.com>
2024-01-12 16:28:18 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 132 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> house;
+int n, cnt = 0;
+
+int dr[3] = {0, 1, 1};
+int dc[3] = {1, 0, 1};
+
+void dfs(int r, int c, int dir){
+  if(r == n && c == n){
+    cnt++;
+    return;
+  }
+
+  bool diagonal = true;
+  for(int i = 0; i < 3; i++){
+    int nr = r + dr[i];
+    int nc = c + dc[i];
+    if(nr > n || nc > n || house[nr][nc]){
+      diagonal = false;
+      continue;
+    }
+    if(i == 2){
+      break;
+    }
+    if(dir == 2 || dir == i){
+      dfs(nr, nc, i);
+    }
+  }
+  if(diagonal){
+    dfs(r + 1, c + 1, 2);
+  }
+  return;
+}
+
+int main(){
+  cin >> n;
+  house.assign(n + 1, vector<int>(n + 1, 0));
+  for(int i = 1; i <= n; i++){
+    for(int j = 1; j <= n; j++){
+      cin >> house[i][j];
+    }
+  }
+
+  dfs(1, 2, 0);
+  cout << cnt;
+  return 0;
+}
