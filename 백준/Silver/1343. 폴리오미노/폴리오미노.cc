945c64d0e614632104f99b5d7f63ea92d12dee03
Koohi45 <86870312+Koohi45@users.noreply.github.com>
2024-02-06 23:11:16 +0900

    [Silver V] Title: 폴리오미노, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.cc
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.cc
@@ -3,31 +3,30 @@
 using namespace std;
 
 string polynomio(string input){
-    int index = 0, cnt = 0;
+    int cnt = 0;
     string result = "";
 
-    while(index < input.length()){
-        while(index < input.length() && input[index] == 'X'){
+    input += " ";
+    for(int i = 0; i < input.length() - 1; i++){
+        if(input[i] == 'X'){
             cnt++;
-            index++;
+        } else if(input[i] == '.'){
+            if(cnt % 2){
+                return "-1";
+            }
+            result += '.';
+            cnt = 0;
         }
-        if(cnt % 2){
-            return "-1";
-        }
-        while(cnt >= 4){
+        if(cnt == 4){
             result += "AAAA";
-            cnt -= 4;
+            cnt = 0;
         }
-        if(cnt == 2){
+        if(cnt == 2 && input[i + 1] != 'X'){
             result += "BB";
             cnt = 0;
         }
-        if(index <input.length() && input[index++] == '.'){
-            result += '.';
-        } 
     }
-    
-    return result;
+    return cnt % 2? "-1" : result;
 }
 
 int main(){
