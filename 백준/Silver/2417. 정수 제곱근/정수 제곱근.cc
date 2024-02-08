dc157fad5e55e6bf95bd932598a09a09d57dd8b0
Koohi45 <86870312+Koohi45@users.noreply.github.com>
2024-02-08 17:12:30 +0900

    [Silver IV] Title: 정수 제곱근, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+
+ll findSqrt(ll left, ll right, ll target){
+    while(left <= right){
+        ll mid = (left + right) / 2;
+        ll squared = mid * mid;
+        if(target == squared){
+            return mid;
+        } else if(target > squared){
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+}
+
+int main(){
+    ll num;
+    cin >> num;
+    cout << findSqrt(0, (ll)sqrt(num), num);
+    return 0;
+}
