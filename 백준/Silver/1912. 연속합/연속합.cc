d5bd614222fa40e2c5b6492951e3331b8a110d9f
Koohi45 <86870312+Koohi45@users.noreply.github.com>
2024-02-14 11:33:43 +0900

    [Silver II] Title: 연속합, Time: 28 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -3,17 +3,14 @@
 
 using namespace std;
 
-const int MAX = 1e8;
-
 int getMaxPrefixSum(int n, vector<int> &num){
-    vector<int> dp(n + 1, 0);
-
-    int min_sum = 0;
-    int max_sum = -MAX;
-    for(int i = 1; i <= n; i++){
-        dp[i] = dp[i - 1] + num[i];
-        max_sum = max(max_sum, dp[i] - min_sum);
-        min_sum = min(min_sum, dp[i]);
+    vector<int> dp(n, 0);
+    int max_sum = num[0];
+    
+    dp[0] = num[0];
+    for(int i = 1; i < n; i++){
+        dp[i] = max(dp[i - 1] + num[i], num[i]);
+        max_sum = max(max_sum, dp[i]);
     }
     return max_sum;
 }
@@ -22,8 +19,8 @@ int main(){
     int n;
 
     cin >> n;
-    vector<int> num(n + 1, 0);
-    for(int i = 1; i <= n; i++){
+    vector<int> num(n, 0);
+    for(int i = 0; i < n; i++){
         cin >> num[i];
     }
 
