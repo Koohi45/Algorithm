13bc6824b2ce5919059075f4593c48693c24dee1
Koohi45 <86870312+Koohi45@users.noreply.github.com>
2024-02-21 11:00:04 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -4,40 +4,27 @@ using namespace std;
 
 int n, s, cnt = 0;
 vector<int> num;
-vector<bool> chosen;
 
-void backtracking(int idx){
+void backtracking(int idx, int curr){
     if(idx == n){
-        int sum = 0;
-        bool flag = false;
-        for(int i = 0; i < n; i++){
-            if(chosen[i]){
-                sum += num[i];
-                flag = true;
-            }
-        }
-        if(sum == s && flag){
-            cnt++;
-        }
         return;
     }
-
-    chosen[idx] = true;
-    backtracking(idx + 1);
-    chosen[idx] = false;
-    backtracking(idx + 1);
+    if(curr + num[idx] == s){
+        cnt++;
+    }
+    backtracking(idx + 1, curr + num[idx]);
+    backtracking(idx + 1, curr);
 }
 
 int main(){
     cin >> n >> s;
 
     num.assign(n, 0);
-    chosen.assign(n, false);
     for(int i = 0; i < n; i++){
         cin >> num[i];
     }
 
-    backtracking(0);
+    backtracking(0, 0);
     cout << cnt;
     return 0;
 }
