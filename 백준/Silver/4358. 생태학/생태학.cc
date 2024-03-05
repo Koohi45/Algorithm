3fc8b52e9bd1733149309715c0d5fa9300a4eb83
Koohi45 <86870312+Koohi45@users.noreply.github.com>
2024-03-05 17:12:34 +0900

    [Silver II] Title: 생태학, Time: 516 ms, Memory: 2296 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int main(){
+    int cnt = 0;
+    string tree;
+    set<string> tree_names;
+    map<string, double> tree_count;
+    
+    while(getline(cin, tree)){
+        if(tree == ""){
+            break;
+        }
+        cnt++;
+        tree_names.insert(tree);
+        tree_count[tree]++;
+    }
+
+    cout << fixed;
+    cout.precision(4);
+    for(auto iter: tree_names){
+        cout << iter << " " << tree_count[iter] / cnt * 100 << '\n';
+    }
+    return 0;
+}
