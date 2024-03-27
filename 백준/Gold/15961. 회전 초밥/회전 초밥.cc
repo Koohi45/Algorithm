af9d9a3d1738f79e56c10fc3709562b1ccdeeed8
Koohi45 <86870312+Koohi45@users.noreply.github.com>
2024-03-27 12:56:03 +0900

    [Gold IV] Title: 회전 초밥, Time: 976 ms, Memory: 13740 KB -BaekjoonHub

---
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int maxSushi(int n, int d, int k, int c, vector<int> &belt){
+    vector<int> types(d + 1, 0);
+    
+    int curr_types = 1; 
+    types[c]++;
+
+    for(int i = 0; i < k; i++){
+        if(!types[belt[i]]++){
+            curr_types++;
+        }
+    }
+
+    int max_types = curr_types;
+    for(int i = k; i < n + k - 1; i++){
+        types[belt[i - k]]--;
+        if(!types[belt[i - k]]){
+            curr_types--;
+        }
+        if(!types[belt[i % n]]){
+            curr_types++;
+        }
+        types[belt[i % n]]++;
+        max_types = max(max_types, curr_types);
+    }
+
+    return max_types;
+}
+
+int main(){
+    int n, d, k, c;
+    cin >> n >> d >> k >> c;
+
+    vector<int> belt(n);
+    for(int i = 0; i < n; i++){
+        cin >> belt[i];
+    }
+
+    cout << maxSushi(n, d, k, c, belt);
+    return 0;
+}
