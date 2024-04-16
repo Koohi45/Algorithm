d4dc69eb8f26e1ad175433739320acf31ed32b0e
Koohi45 <86870312+Koohi45@users.noreply.github.com>
2024-04-16 10:30:37 +0900

    [Silver I] Title: 스티커, Time: 300 ms, Memory: 3980 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int maxStickers(int n, vector<vector<int>> &stickers){
+    vector<vector<int>> dp(2, vector<int>(n + 1, 0));
+    dp[0][1] = stickers[0][1];
+    dp[1][1] = stickers[1][1];
+
+    for(int i = 2; i <= n; i++){
+        dp[0][i] = max(dp[1][i - 1], dp[1][i - 2]) + stickers[0][i];
+        dp[1][i] = max(dp[0][i - 1], dp[0][i - 2]) + stickers[1][i];
+    }
+
+    return max(dp[0][n], dp[1][n]);
+}
+
+int main(){
+    int t, n;
+    cin >> t;
+    while(t--){
+        cin >> n;
+        vector<vector<int>> stickers(2, vector<int>(n + 1));
+        for(int i = 0; i < 2; i++){
+            for(int j = 1; j <= n; j++){
+                cin >> stickers[i][j];
+            }
+        }
+        cout << maxStickers(n, stickers) << '\n';
+    }
+    return 0;
+}
